c99f5177787f15d6cb2df65a457ef603b6eb4d37
Avinash-0612 <avinashchinnabattuni@gmail.com>
2026-01-31 23:55:20 -0600

    Update variables.tf
    # Variables for Terraform deployment
    # Set these in terraform.tfvars or as environment variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,1 +1,30 @@
+# Variables for Terraform deployment
+# Set these in terraform.tfvars or as environment variables
 
+variable "synapse_sql_password" {
+  description = "Password for Synapse SQL Administrator"
+  type        = string
+  sensitive   = true
+}
+
+variable "aad_admin_object_id" {
+  description = "Object ID of Azure AD Admin for Synapse"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure AD Tenant ID"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "production"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
